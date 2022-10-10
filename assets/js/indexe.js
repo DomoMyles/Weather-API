9ac9c052d471097a6e26e37c44c2a39f460d6212
DomoMyles <47869842+DomoMyles@users.noreply.github.com>
2022-10-10 16:09:10 -0400

    Update

--- assets/js/indexe.js
+++ assets/js/indexe.js
@@ -7,7 +7,7 @@ var long = 0
 function getInput() {
     var citySearch = document.querySelector(".input");
     var cityName = citySearch.value;
-    var latlon = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=20&appid=" + key;
+    var latlon = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=20&appid=" + key;
     fetch(latlon)
         .then(function (response) {
             return response.json();
